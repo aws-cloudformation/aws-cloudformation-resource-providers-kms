0222af88d7fef780cd47a1a161ada8e70b771906
Anton Mokhovikov <ammokhov@amazon.com>
2020-04-16 12:02:00 -0700

    Use Static HTTP Client

--- key/src/main/java/software/amazon/kms/key/ClientBuilder.java
+++ key/src/main/java/software/amazon/kms/key/ClientBuilder.java
@@ -1,9 +1,13 @@
 package software.amazon.kms.key;
 
 import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.cloudformation.LambdaWrapper;
 
 public class ClientBuilder {
-    private static class LazyHolder { static final KmsClient KMS_CLIENT = KmsClient.create();}
+    private static class LazyHolder { static final KmsClient KMS_CLIENT = KmsClient.builder()
+        .httpClient(LambdaWrapper.HTTP_CLIENT)
+        .build();
+    }
     public static KmsClient getClient() {
         return LazyHolder.KMS_CLIENT;
     }
