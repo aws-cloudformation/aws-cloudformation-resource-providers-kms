0d34f89462b16d19c97fb5f2231bdae7a32ac2be
Anton Mokhovikov <ammokhov@amazon.com>
2020-05-18 14:36:47 -0700

    modified list handler

--- key/src/main/java/software/amazon/kms/key/ListHandler.java
+++ key/src/main/java/software/amazon/kms/key/ListHandler.java
@@ -1,25 +1,25 @@
 package software.amazon.kms.key;
 
+import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.kms.model.ListKeysResponse;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ListHandler extends BaseHandler<CallbackContext> {
-
-    @Override
-    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+public class ListHandler extends BaseHandlerStd {
+    protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
         final AmazonWebServicesClientProxy proxy,
         final ResourceHandlerRequest<ResourceModel> request,
         final CallbackContext callbackContext,
+        final ProxyClient<KmsClient> proxyClient,
         final Logger logger) {
-
-        final ListKeysResponse listKeysResponse = proxy.injectCredentialsAndInvokeV2(Translator.listKeysRequest(request.getNextToken()), ClientBuilder.getClient()::listKeys);
+        final ListKeysResponse listKeysResponse = proxy.injectCredentialsAndInvokeV2(Translator.listKeysRequest(request.getNextToken()), proxyClient.client()::listKeys);
 
         final List<ResourceModel> models = listKeysResponse
             .keys()
