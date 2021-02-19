3cf2cb215250a30d1116ddce4168aa1d7dc2a177
John Cullen <jcul@amazon.com>
2021-02-19 11:18:09 -0800

    Fix indentation in BaseHandlerStd

--- alias/src/main/java/software/amazon/kms/alias/BaseHandlerStd.java
+++ alias/src/main/java/software/amazon/kms/alias/BaseHandlerStd.java
@@ -7,53 +7,54 @@ import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
-public abstract class BaseHandlerStd extends BaseHandler<CallbackContext>  {
-  protected static final int CALLBACK_DELAY_SECONDS = 60;
+public abstract class BaseHandlerStd extends BaseHandler<CallbackContext> {
+    protected static final int CALLBACK_DELAY_SECONDS = 60;
 
-  final AliasHelper aliasHelper;
+    final AliasHelper aliasHelper;
 
-  public BaseHandlerStd() {
-    this(new AliasHelper());
-  }
-
-  public BaseHandlerStd(final AliasHelper aliasHelper) {
-    // Allows for mocking alias helper in our unit tests
-    this.aliasHelper = aliasHelper;
-  }
-
-  @Override
-  public final ProgressEvent<ResourceModel, CallbackContext> handleRequest(final AmazonWebServicesClientProxy proxy,
-      final ResourceHandlerRequest<ResourceModel> request,
-      final CallbackContext callbackContext,
-      final Logger logger) {
-    return handleRequest(
-        proxy,
-        request,
-        callbackContext != null ? callbackContext : new CallbackContext(),
-        proxy.newProxy(ClientBuilder::getClient),
-        logger);
-  }
-
-  protected abstract ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-      AmazonWebServicesClientProxy proxy,
-      ResourceHandlerRequest<ResourceModel> request,
-      CallbackContext callbackContext,
-      ProxyClient<KmsClient> proxyClient,
-      Logger logger);
-
-  /**
-   * Perform the final propagation delay to make sure the latest
-   * changes to the alias are available throughout the region.
-   */
-  protected static ProgressEvent<ResourceModel, CallbackContext> propagate(
-      final ProgressEvent<ResourceModel, CallbackContext> progressEvent) {
-    final CallbackContext callbackContext = progressEvent.getCallbackContext();
-    if (callbackContext.isPropagated()) {
-      return progressEvent;
+    public BaseHandlerStd() {
+        this(new AliasHelper());
     }
 
-    callbackContext.setPropagated(true);
-    return ProgressEvent.defaultInProgressHandler(callbackContext, CALLBACK_DELAY_SECONDS,
+    public BaseHandlerStd(final AliasHelper aliasHelper) {
+        // Allows for mocking alias helper in our unit tests
+        this.aliasHelper = aliasHelper;
+    }
+
+    @Override
+    public final ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
+        return handleRequest(
+            proxy,
+            request,
+            callbackContext != null ? callbackContext : new CallbackContext(),
+            proxy.newProxy(ClientBuilder::getClient),
+            logger);
+    }
+
+    protected abstract ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        AmazonWebServicesClientProxy proxy,
+        ResourceHandlerRequest<ResourceModel> request,
+        CallbackContext callbackContext,
+        ProxyClient<KmsClient> proxyClient,
+        Logger logger);
+
+    /**
+     * Perform the final propagation delay to make sure the latest
+     * changes to the alias are available throughout the region.
+     */
+    protected static ProgressEvent<ResourceModel, CallbackContext> propagate(
+        final ProgressEvent<ResourceModel, CallbackContext> progressEvent) {
+        final CallbackContext callbackContext = progressEvent.getCallbackContext();
+        if (callbackContext.isPropagated()) {
+            return progressEvent;
+        }
+
+        callbackContext.setPropagated(true);
+        return ProgressEvent.defaultInProgressHandler(callbackContext, CALLBACK_DELAY_SECONDS,
             progressEvent.getResourceModel());
-  }
+    }
 }
